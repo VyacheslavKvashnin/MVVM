35b8aa1bc2aea81ccaee045dbf9565c13b824dee
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-02 13:10:00 +0500

    Start project

---
+++ MVVM/GreetingViewModelProtocol.swift
@@ -0,0 +1,36 @@
+//
+//  GreetingViewModelProtocol.swift
+//  MVVM
+//
+//  Created by Вячеслав Квашнин on 02.05.2022.
+//
+
+import Foundation
+
+protocol GreetingViewModelProtocol {
+    var greeting: String { get }
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)? { get set }
+    init(person: Person)
+    func showGreeting()
+}
+
+class GreetingViewModel: GreetingViewModelProtocol {
+    var greeting: String? {
+        didSet {
+            greetingDidChange?(self)
+        }
+    }
+    
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)?
+    
+    private let person: Person
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func showGreeting() {
+        greeting = "Hello \(person.name) \(person.surname)"
+    }
+    
+    
+}
